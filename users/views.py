98dae3036d132e2c582957e2e443f3d10844591e
MusulmanM <mmmmmmvvv200@gmail.com>
2026-03-01 23:44:22 +0500

    YOUR TEXT

--- users/views.py
+++ users/views.py
@@ -2,6 +2,10 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import User
 from .serializer import UserSerializer
+# Create your views here.
+
+
+
 
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
