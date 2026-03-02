847f606a9a81c672d4169784e9dbed2d4316cfea
MusulmanM <mmmmmmvvv200@gmail.com>
2026-03-02 12:01:25 +0500

    deploy

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Xush kelibsiz!")
+    return HttpResponse("Xush kelibsiz! Musulman ;).")
 
 
 
