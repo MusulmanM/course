b6facd2364df8ac09de0fb861721484f5b930dd0
MusulmanM <mmmmmmvvv200@gmail.com>
2026-03-02 11:59:40 +0500

    deploy

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Xush kelibsiz!")
+
+
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/student/', include('student.urls')),
